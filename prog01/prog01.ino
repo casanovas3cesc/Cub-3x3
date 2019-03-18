369fb7f87a34855d6d6161edc7f0516a7eabf563
casanovas3cesc <cesc.casanovas3@gmail.com>
2019-03-18 18:49:26 +0100

    Update prog01.ino
    definitiu sense errors

--- prog01/prog01.ino
+++ prog01/prog01.ino
@@ -1,79 +1,451 @@
 /**********************************************************************************
 **                                                                               **
-**                              Array de 8 LEDs                                  **
+**                             Cub de 3x3x3 LEDs                                 **
 **                                                                               **
 **                                                                               **
 **********************************************************************************/
+
 //********** Includes *************************************************************
+
 //********** Variables ************************************************************
-const int col_9 = 2;          // donar nom al pin 5 de l’Arduino
-const int col_8 = 3;          // donar nom al pin 6 de l’Arduino
-const int col_7 = 4;          // donar nom al pin 7 de l’Arduino
-const int col_6 = 5;          // donar nom al pin 8 de l’Arduino
-const int col_5 = 6;          // donar nom al pin 9 de l’Arduino
-const int col_4 = 7;         // donar nom al pin 10 de l’Arduino
-const int col_3 = 8;         // donar nom al pin 11 de l’Arduino
-const int col_2 = 9;         // donar nom al pin 12 de l’Arduino
-const int col_1 = 10;
-const int trans_3 = 13;
-const int trans_2 = 12;
-const int trans_1 = 11;
-unsigned long temps = 1000;
+const int col1 = 10;          
+const int col2 = 9;          
+const int col3 = 8;          
+const int col4 = 7;          
+const int col5 = 6;        
+const int col6 = 5;         
+const int col7 = 4;        
+const int col8 = 3;         
+const int col9 = 2;                   
+const int tran1 = 11;         
+const int tran2 = 12;        
+const int tran3 = 13;
+int vel = 500;         
+
 //********** Setup ****************************************************************
 void setup()
 {
-  pinMode(col_9, OUTPUT);     
-  pinMode(col_8, OUTPUT);     
-  pinMode(col_7, OUTPUT);     
-  pinMode(col_6, OUTPUT);   
-  pinMode(col_5, OUTPUT);   
-  pinMode(col_4, OUTPUT);   
-  pinMode(col_3, OUTPUT);     
-  pinMode(col_2, OUTPUT);    
-  pinMode(col_1, OUTPUT); 
-  pinMode(trans_3, OUTPUT);
-  pinMode(trans_2, OUTPUT); 
-  pinMode(trans_1, OUTPUT);  
+  pinMode(col1, OUTPUT);     
+  pinMode(col2, OUTPUT);     
+  pinMode(col3, OUTPUT);     
+  pinMode(col4, OUTPUT);    
+  pinMode(col5, OUTPUT);   
+  pinMode(col6, OUTPUT);    
+  pinMode(col7, OUTPUT);     
+  pinMode(col8, OUTPUT);     
+  pinMode(col9, OUTPUT);    
+  pinMode(tran1, OUTPUT);    
+  pinMode(tran2, OUTPUT);     
+  pinMode(tran3, OUTPUT);        
 }
+
 //********** Loop *****************************************************************
 void loop()
 {
- 
-  digitalWrite(col_9, HIGH);     
-  digitalWrite(col_8, HIGH);     
-  digitalWrite(col_7, HIGH);     
-  digitalWrite(col_6, HIGH);   
-  digitalWrite(col_5, HIGH);   
-  digitalWrite(col_4, HIGH);   
-  digitalWrite(col_3, HIGH);     
-  digitalWrite(col_2, HIGH);    
-  digitalWrite(col_1, HIGH); 
-  digitalWrite(trans_3, HIGH);
-  digitalWrite(trans_2, HIGH); 
-  digitalWrite(trans_1, HIGH);  
+  digitalWrite(col1, HIGH);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, HIGH);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, LOW);     
   
-  delay(temps);               
+  delay(vel);                 
   
-   digitalWrite(col_9, LOW);     
-  digitalWrite(col_8, LOW);     
-  digitalWrite(col_7, LOW);     
-  digitalWrite(col_6, HIGH);   
-  digitalWrite(col_5, LOW);   
-  digitalWrite(col_4, LOW);   
-  digitalWrite(col_3, LOW);     
-  digitalWrite(col_2, LOW);    
-  digitalWrite(col_1, LOW); 
-  digitalWrite(trans_3, HIGH);
-  digitalWrite(trans_2, LOW); 
-  digitalWrite(trans_1, LOW);  
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, HIGH);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, HIGH);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, LOW);     
   
-  delay(temps);        
+  delay(vel);  
   
- 
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, HIGH);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, HIGH);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel);
 
-
-
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, HIGH);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, HIGH);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, LOW);     
   
-          }
- 
+  delay(vel); 
+  
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, HIGH);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, HIGH);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel);
+  
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, HIGH);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, HIGH);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel); 
+   
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, HIGH);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, HIGH);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel);
+    
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, HIGH);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, HIGH);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel);  
+  
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, HIGH);    
+  digitalWrite(tran1, HIGH);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel); 
+   
+  digitalWrite(col1, HIGH);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, HIGH);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel);  
+  
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, HIGH);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, HIGH);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel);
+    
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, HIGH);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, HIGH);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel); 
+   
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, HIGH);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, HIGH);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel); 
+   
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, HIGH);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, HIGH);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel);
+    
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, HIGH);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, HIGH);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel); 
+   
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, HIGH);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, HIGH);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel);
+    
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, HIGH);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, HIGH);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel);
+    
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, HIGH);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, HIGH);    
+  digitalWrite(tran3, LOW);     
+  
+  delay(vel);  
+  
+  digitalWrite(col1, HIGH);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, HIGH);     
+  
+  delay(vel);
+    
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, HIGH);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, HIGH);     
+  
+  delay(vel); 
+   
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, HIGH);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, HIGH);     
+  
+  delay(vel);
+    
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, HIGH);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, HIGH);     
+  
+  delay(vel); 
+   
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, HIGH);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, HIGH);     
+  
+  delay(vel);  
+  
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, HIGH);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, HIGH);     
+  
+  delay(vel);  
+  
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, HIGH);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, HIGH);     
+  
+  delay(vel);
+    
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, HIGH);   
+  digitalWrite(col9, LOW);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, HIGH);     
+  
+  delay(vel); 
+   
+  digitalWrite(col1, LOW);    
+  digitalWrite(col2, LOW);    
+  digitalWrite(col3, LOW);    
+  digitalWrite(col4, LOW);    
+  digitalWrite(col5, LOW);    
+  digitalWrite(col6, LOW);    
+  digitalWrite(col7, LOW);    
+  digitalWrite(col8, LOW);   
+  digitalWrite(col9, HIGH);    
+  digitalWrite(tran1, LOW);    
+  digitalWrite(tran2, LOW);    
+  digitalWrite(tran3, HIGH);     
+  
+  delay(vel);     
+}
+
 //********** Funcions *************************************************************
